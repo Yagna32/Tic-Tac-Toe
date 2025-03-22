0ed30679e96ed8cb80588cad4c26b894e11fec3d
yagna <yagnapatelhirenk@gmail.com>
2025-03-22 17:00:57 +0530

    added Dockerfile

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    host: "0.0.0.0", // Allow access from outside the container
+    port: 5173,      // Make sure it matches the EXPOSE command in Dockerfile
+    strictPort: true
+  }
 });
